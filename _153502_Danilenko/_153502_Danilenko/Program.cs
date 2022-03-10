64b618a157c4b7b13f25ea2a9b4c15e84298626f
Shad0wSeeker <91159392+Shad0wSeeker@users.noreply.github.com>
2022-03-10 13:43:44 +0300

    Добавьте файлы проекта.

---
+++ _153502_Danilenko/_153502_Danilenko/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _153502_Danilenko
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter 2 nubmers: ");
+            a = Int32.Parse(Console.ReadLine());
+            b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("The result of division: ");
+            double c = (double)a / b;
+            Console.WriteLine(c);
+        }
+    }
+}
